d903cd5926e7cd6724ac3e0cab9ac4eb1f751555
diyavakil <81391236+diyavakil@users.noreply.github.com>
2026-06-13 13:54:47 -0700

    Update image upload formats in streamlit_app.py
    Expanded the list of supported image formats for upload.

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ except Exception as e:
     st.stop()
 
 # upload img
-uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png", "heic"], help="Supported formats: HEIC, PNG, JPG, JPEG")
+uploaded_file = st.file_uploader("Upload an image", type=["heic", "png", "jpeg", "jpg", "bmp", "webp", "dng", "avif", "jp2", "mpo", "pfm"], help="Supported formats: HEIC, PNG, JPEG, JPG, BMP, WEBP, DNG, AVIF, JP2, MPO, PFM")
 if uploaded_file is not None:
     try:
         with st.spinner("Loading image..."): 
